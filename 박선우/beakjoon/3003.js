f88df80213ba9d0dcb1f98a9f821b2fbea246eef
Sounwoo <mna800@naver.com>
2022-08-12 17:13:29 +0900

    08.12

---
+++ 박선우/beakjoon/3003.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const input = fs.readFileSync('input.txt').toString().split(' ');
+
+function anwer() {
+    const price = [1, 1, 2, 2, 2, 8];
+    const idx = [];
+
+    for (let i = 0; i < price.length; i++) {
+        if (input[i] !== price[i]) {
+            idx.push(price[i] - input[i]);
+        } else if (input[i] === price[i]) {
+            idx.push(price[i]);
+        }
+    }
+    return idx.toString().replace(/,/g, ' ');
+}
+
+console.log(anwer());
